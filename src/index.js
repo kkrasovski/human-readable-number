c5a625e10b2d433697c4cf2f4effde535d7856b4
Kiryl Krasouski <ziksow@gmail.com>
2021-02-25 20:15:35 +0300

    init: add first version of the code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    // function toReadable(number) {
+    let numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let numbersDozens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    let hundredsResult;
+    let dozensResult;
+    let allDozen = number - (Math.floor(number / 100)) * 100;  
+    let dozenMore20 = numbersDozens[Math.floor((allDozen / 10) - 2)];
+    let dozenRemainder = numbers[allDozen - Math.floor((allDozen / 10)) * 10 - 1]
+    let hundredsT = " hundred";
+    if (number / 100 >= 1 && number % 100 == 0) {
+        let hundredsN = numbers[Math.floor(number / 100 - 1)];
+        hundredsResult = hundredsN + hundredsT;
+    }
+    else if (number / 100 >= 1) {
+        let hundredsN = numbers[Math.floor(number / 100 - 1)];
+        hundredsResult = hundredsN + hundredsT + " ";
+    }
+    else if (number % 10) {
+        hundredsResult = "";
+    }
+    else {
+        hundredsResult = "";
+    }
+    if (allDozen == 0) {
+        dozensResult = "";
+    }
+    else if (allDozen >= 20) {
+        if (!(allDozen % 10)) {
+            dozensResult = dozenMore20;
+        } else {
+            dozensResult = dozenMore20 + ' ' + dozenRemainder;
+        }
+    } else {
+        dozensResult = numbers[allDozen - 1]
+    }
+    let mainResult = hundredsResult + dozensResult;
+    if (number == 0) {
+        return "zero"
+    } else {
+        return mainResult
+    }
 }
